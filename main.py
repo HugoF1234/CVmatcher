e70d17fe8b2d3988f94ad43844663d7c675f86ff
HugoF1234 <167479132+HugoF1234@users.noreply.github.com>
2025-06-19 14:57:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from app.routes import app
-
+import os
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
 
