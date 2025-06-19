dd51f980b669cad367743940bc452b6e4e7e6e50
HugoF1234 <167479132+HugoF1234@users.noreply.github.com>
2025-06-19 16:35:13 +0200

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,3 +3,5 @@ import os
 
 template_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'templates'))
 app = Flask(__name__, template_folder=template_dir)
+
+from app import routes  # important pour enregistrer les routes
