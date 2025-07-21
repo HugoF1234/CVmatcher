321b951b52aeb8e737019bfe92e1f37ef87e60e7
HugoF1234 <167479132+HugoF1234@users.noreply.github.com>
2025-07-21 12:43:53 +0200

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ DB_NAME = "CVExtraction"
 COLLECTION_NAME = "CVExtractionCollection"
 FAISS_INDEX_FILE = "faiss_index/cv_index.faiss"
 ID_MAPPING_FILE = "faiss_index/id_mapping.pkl"
-GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyB2w2GCNE6EqvAtcA2Dj9rsvkD2YzFFMfM")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY", "AIzaSyBST1NZu1zU-qffOWtoNn6uhzODaJBviiQ")
 TOP_K = 5
 SECRET_KEY = "0180529a5b9c1ec478296df826a91c31"
 
